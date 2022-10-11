476d70c8b5ce6869418f90474ea5887756ae0b81
shivani-kpipartners <shivani.shukla@kpipartners.com>
2022-10-11 11:26:54 +0530

    Commit to dev

---
+++ Java_Devops/src/practice/Addition.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Addition {
+	public static void main(String args[])   
+	{  
+	int a = 225, b = 115, sum;  
+	sum = a + b;  
+	System.out.println("The sum of " +a+" and "+b + " is: "+sum);  
+	}  
+}
